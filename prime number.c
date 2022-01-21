7becb49eb6ec68496d76adaf7462863c5e9e4d9e
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-01-22 01:44:03 +0530

    #include<stdio.h> int main() {   int n, i, f = 0;   scanf("%d", &n);   for (i = 2; i <= n / 2; ++i)   {     if (n%i==0)     {       f = 1;       break;     }   }
        if (f == 0)
          printf("prime");
        else
          printf("not a prime");
    
      return 0;
    }

---
+++ prime number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int n, i, f = 0;
+  scanf("%d", &n);
+  for (i = 2; i <= n / 2; ++i) 
+  {
+    if (n%i==0) 
+    {
+      f = 1;
+      break;
+    }
+  }
+
+    if (f == 0)
+      printf("prime");
+    else
+      printf("not a prime");
+
+  return 0;
+}
