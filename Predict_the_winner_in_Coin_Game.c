71bbaf73aa7ab06df221eff5919208243ba33b25
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-01-22 23:15:45 +0530

    #include<stdio.h> int main() {     int m,n;     scanf("%d%d",&m,&n);     if(m%2==0 ||n%2==0)     printf("Player 1");     else     printf("Player 2");     return 0; }

--- Predict_the_winner_in_Coin_Game.c
+++ Predict_the_winner_in_Coin_Game.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int M,N;
-    scanf("%d%d",&M,&N);
-    if(M%2==0 ||N%2==0)
+    int m,n;
+    scanf("%d%d",&m,&n);
+    if(m%2==0 ||n%2==0)
     printf("Player 1");
     else
     printf("Player 2");
