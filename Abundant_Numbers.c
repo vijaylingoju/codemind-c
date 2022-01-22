25953b15252399180703d6948bd25dbea9a24a0c
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-01-22 21:38:06 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,s=0;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n%i==0)         s=s+i;     }     if(s>n)     printf("True");     else     printf("False");
         return 0;
    }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,s=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        s=s+i;
+    }
+    if(s>n)
+    printf("True");
+    else
+    printf("False");
+    
+     return 0;
+}
