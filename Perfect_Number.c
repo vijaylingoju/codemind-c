18904fc74164092edbf7150ac0988cdaa6925725
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-01-24 00:00:13 +0530

    #include<stdio.h> int main() {     int n,f,i,s=0;     scanf("%d",&n);     for(i=1;i<=n/2;i++)     {         if(n%i==0)         s=s+i;     }     if(s==n)     printf("True");     else     printf("False");     return 0; }

---
+++ Perfect_Number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,f,i,s=0;
+    scanf("%d",&n);
+    for(i=1;i<=n/2;i++)
+    {
+        if(n%i==0)
+        s=s+i;
+    }
+    if(s==n)
+    printf("True");
+    else
+    printf("False");
+    return 0;
+}
