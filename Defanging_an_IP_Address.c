cb89f8dda3e025d9faaab10899fc22962c2f9f97
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-01-25 17:16:02 +0530

    #include<stdio.h> int main() {     char s[100];     int i,x=0,y=0,j;     scanf("%s",s);    // scanf("%d%d",&n1,&n2);     for(i=0;s[i]!=NULL;i++);     for(j=0;j<i;j++)         {             if(s[j]!='.')             printf("%c",s[j]);             else             printf("[.]");         }     return 0; }

---
+++ Defanging_an_IP_Address.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char s[100];
+    int i,x=0,y=0,j;
+    scanf("%s",s);
+   // scanf("%d%d",&n1,&n2);
+    for(i=0;s[i]!=NULL;i++);
+    for(j=0;j<i;j++)
+        {
+            if(s[j]!='.')
+            printf("%c",s[j]);
+            else
+            printf("[.]");
+        }
+    return 0;
+}
