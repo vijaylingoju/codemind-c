fd7365741c895b10c2913217ed4155fe532f2619
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-04-30 19:35:47 +0530

    #include<stdio.h> int main() {     int arr[100000],i,j,k,n,t=0,q;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     q=n;     if(n>=4&&n<100)     {         for(i=0;i<n;i++)         {             for(j=i+1;j<q;j++)             {                 if(arr[i]==arr[j])                 {                     for(k=j;k<n;k++)                     {                         arr[k]=arr[k+1];                     }                     j--;                     q--;                 }             }         }         for(i=0;i<q;i++)         {             for(j=i+1;j<q;j++)             {                 if(arr[i]>arr[j])                 {                     t=arr[i];                     arr[i]=arr[j];                     arr[j]=t;                 }             }         }         if(q>=3)         printf("%d",arr[q-3]);         else         printf("It is not possible");     }     else     {         printf("It is not possible");     } }

---
+++ Print_the_third_largest_number_in_the_array.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100000],i,j,k,n,t=0,q;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    q=n;
+    if(n>=4&&n<100)
+    {
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<q;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    for(k=j;k<n;k++)
+                    {
+                        arr[k]=arr[k+1];
+                    }
+                    j--;
+                    q--;
+                }
+            }
+        }
+        for(i=0;i<q;i++)
+        {
+            for(j=i+1;j<q;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    t=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=t;
+                }
+            }
+        }
+        if(q>=3)
+        printf("%d",arr[q-3]);
+        else
+        printf("It is not possible");
+    }
+    else
+    {
+        printf("It is not possible");
+    }
+}
